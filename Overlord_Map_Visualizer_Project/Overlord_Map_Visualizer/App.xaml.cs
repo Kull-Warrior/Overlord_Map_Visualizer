f4a267fb31b3328c14dd56b07f929d4db8b231e4
Kull-Warrior <92987811+Kull-Warrior@users.noreply.github.com>
2023-10-16 20:26:35 +0200

    Initial commit

---
+++ Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Overlord_Map_Visualizer
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
