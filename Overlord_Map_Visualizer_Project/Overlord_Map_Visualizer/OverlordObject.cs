4ae3b84a93ac1ec3074c56294e5c1d9c585edd79
Wehner, Tobias <tobias992000@gmail.com>
2024-04-18 12:08:28 +0200

    Created OverlordObject class and OverlordObject type

---
+++ Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/OverlordObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overlord_Map_Visualizer
+{
+    enum OverlordObjectType
+    {
+        BrownMinionGate,
+        RedMinionGate,
+        GreenMinionGate,
+        BlueMinionGate,
+        TowerGate
+    }
+    class OverlordObject
+    {
+        private int X { get; set; }
+        private int Y { get; set; }
+        private int Z { get; set; }
+        private OverlordObjectType Type { get; set; }
+
+        public OverlordObject(int x, int y, int z, OverlordObjectType type)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Type = type;
+        }
+    }
+}
