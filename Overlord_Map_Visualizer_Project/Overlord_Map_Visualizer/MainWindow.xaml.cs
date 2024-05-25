9674ad26d33da1cfe8186c32f903dd77a9c67cde
Kull-Warrior <tobias992000@gmail.com>
2024-05-25 12:56:55 +0200

    Updated GetMapDataOffset Function
    The function GetMapDataOffset now works for all omp files and is no longer hardcoded only for Overlord 1

--- Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/MainWindow.xaml.cs
+++ Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/MainWindow.xaml.cs
@@ -711,125 +711,41 @@ namespace Overlord_Map_Visualizer
 
         private int GetMapDataOffset()
         {
-            switch (OMPFilePathString)
+            int srcOffset = 200 + MapWidth * MapHeight * 4;
+            int totalNumberOfBytes = 500;
+            byte[] src = new byte[totalNumberOfBytes];
+
+            using (BinaryReader reader = new BinaryReader(new FileStream(OMPFilePathString, FileMode.Open)))
             {
-                case string a when a.Contains("Exp - HalflingMain"):
-                    return 408;
-                case string a when a.Contains("Exp - Halfling Abyss"):
-                    return 394;
-                case string a when a.Contains("Exp - ElfMain"):
-                    return 416;
-                case string a when a.Contains("Exp - Elf Abyss"):
-                    return 378;
-                case string a when a.Contains("Exp - PaladinMain"):
-                    return 405;
-                case string a when a.Contains("Exp - Paladin Abyss"):
-                    return 405;
-                case string a when a.Contains("Exp - DwarfMain"):
-                    return 386;
-                case string a when a.Contains("Exp - Dwarf Abyss"):
-                    return 403;
-                case string a when a.Contains("Exp - WarriorMain"):
-                    return 378;
-                case string a when a.Contains("Exp - Warrior Abyss - 01"):
-                    return 416;
-                case string a when a.Contains("Exp - Warrior Abyss - 02"):
-                    return 400;
-                case string a when a.Contains("Exp - Tower_Dungeon"):
-                    return 403;
-                case string a when a.Contains("Exp - Tower_Spawnpit"):
-                    return 424;
-                case string a when a.Contains("Exp - Tower"):
-                    return 409;
-                case string a when a.Contains("HalflingMain"):
-                    return 400;
-                case string a when a.Contains("SlaveCamp"):
-                    return 378;
-                case string a when a.Contains("HalflingHomes1of2"):
-                    return 378;
-                case string a when a.Contains("HalflingHomes2of2"):
-                    return 378;
-                case string a when a.Contains("HellsKitchen"):
-                    return 378;
-                case string a when a.Contains("EntryCastleSpree"):
-                    return 402;
-                case string a when a.Contains("SpreeDungeon"):
-                    return 386;
-                case string a when a.Contains("ElfMain"):
-                    return 408;
-                case string a when a.Contains("GreenCave"):
-                    return 394;
-                case string a when a.Contains("SkullDen"):
-                    return 390;
-                case string a when a.Contains("TrollTemple"):
-                    return 386;
-                case string a when a.Contains("PaladinMain"):
-                    return 397;
-                case string a when a.Contains("BlueCave"):
-                    return 409;
-                case string a when a.Contains("Sewers1of2"):
-                    return 410;
-                case string a when a.Contains("Sewers2of2"):
-                    return 408;
-                case string a when a.Contains("Red Light Inn"):
-                    return 403;
-                case string a when a.Contains("Citadel"):
-                    return 407;
-                case string a when a.Contains("DwarfMain"):
-                    return 378;
-                case string a when a.Contains("GoldMine"):
-                    return 378;
-                case string a when a.Contains("Quarry"):
-                    return 378;
-                case string a when a.Contains("HomeyHalls1of2"):
-                    return 370;
-                case string a when a.Contains("HomeyHalls2of2"):
-                    return 370;
-                case string a when a.Contains("ArcaniumMine"):
-                    return 370;
-                case string a when a.Contains("RoyalHalls"):
-                    return 370;
-                case string a when a.Contains("WarriorMain"):
-                    return 370;
-                case string a when a.Contains("2P_Deathtrap"):
-                    return 501;
-                case string a when a.Contains("2P_Gates"):
-                    return 488;
-                case string a when a.Contains("2P_LastStand"):
-                    return 464;
-                case string a when a.Contains("2P_PartyCrashers"):
-                    return 469;
-                case string a when a.Contains("2P_Plunder"):
-                    return 505;
-                case string a when a.Contains("2P_TombRobber"):
-                    return 505;
-                case string a when a.Contains("Tower_Dungeon"):
-                    return 395;
-                case string a when a.Contains("Tower_Spawnpit"):
-                    return 424;
-                case string a when a.Contains("Tower"):
-                    return 401;
-                case string a when a.Contains("PlayerMap"):
-                    return 250;
-                case string a when a.Contains("2P_Arena2"):
-                    return 492;
-                case string a when a.Contains("2P_Bombs"):
-                    return 523;
-                case string a when a.Contains("2P_GrabTheMaidens"):
-                    return 496;
-                case string a when a.Contains("2P_KillTheHoard"):
-                    return 500;
-                case string a when a.Contains("2P_KingoftheHill"):
-                    return 479;
-                case string a when a.Contains("2P_March_Mellow_Maidens"):
-                    return 514;
-                case string a when a.Contains("2P_Misty"):
-                    return 481;
-                case string a when a.Contains("2P_RockyRace"):
-                    return 458;
-                default:
-                    return 0;
+                reader.BaseStream.Seek(srcOffset, SeekOrigin.Begin);
+                reader.Read(src, 0, totalNumberOfBytes);
             }
+
+            byte[] pattern = new byte[9] { (byte)'I', (byte)'n', (byte)'d', (byte)'o', (byte)'o', (byte)'r', (byte)'S', (byte)'e', (byte)'t' };//IndoorSet
+
+            int maxFirstCharSlot = src.Length - pattern.Length + 1;
+            for (int i = 0; i < maxFirstCharSlot;i++)
+            {
+                if (src[i] != pattern[0])
+                {
+                    continue;
+                }
+
+                for (int j = pattern.Length - 1; j >= 1; j--)
+                {
+                    if (src[i + j] != pattern[j])
+                    {
+                        break;
+                    }
+                    if (j== 1)
+                    {
+                        return i + 200 - 4;
+                    }
+                }
+
+            }
+
+            return 0;
         }
 
         private double GetMapWaterLevel()
