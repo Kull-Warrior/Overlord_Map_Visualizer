251189f35a760622dd67aabca825961d6b94411d
Wehner, Tobias <tobias992000@gmail.com>
2024-07-30 15:15:11 +0200

    Enums moved to a resource folder

---
+++ Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/resources/enums/cursorEnums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Overlord_Map_Visualizer
+{
+    public enum CursorMode
+    {
+        Select,
+        Pipette,
+        Square,
+        Circle,
+        Rotate
+    }
+
+    public enum CursorSubMode
+    {
+        Set,
+        Add,
+        Sub
+    }
+}
---
+++ Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/resources/enums/mapEnums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Overlord_Map_Visualizer
+{
+    public enum MapMode
+    {
+        HeightMap,
+        MainTextureMap,
+        WallTextureMap,
+        FoliageMap,
+        UnknownMap,
+        Full,
+        ThreeDimensional
+    }
+}
