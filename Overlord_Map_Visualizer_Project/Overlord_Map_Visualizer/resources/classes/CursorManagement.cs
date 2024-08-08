7fa0681b9919d7e003ebabdaeff9b253fe2ab5c3
Wehner, Tobias <tobias992000@gmail.com>
2024-08-08 11:33:55 +0200

    Added CursorManagement class

---
+++ Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/resources/classes/CursorManagement.cs
@@ -0,0 +1,16 @@
+﻿namespace Overlord_Map_Visualizer
+{
+    class CursorManagement
+    {
+        public CursorMode Mode;
+        public CursorSubMode SubMode;
+        public int CursorDiameter = 51;
+
+        public CursorManagement()
+        {
+            Mode = CursorMode.Select;
+            SubMode = CursorSubMode.Set;
+            CursorDiameter = 51;
+        }
+    }
+}
