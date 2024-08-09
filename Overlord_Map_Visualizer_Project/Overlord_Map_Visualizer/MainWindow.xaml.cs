21e50dbe8a4874ca1f740da028e959d0237a7749
Wehner, Tobias <tobias992000@gmail.com>
2024-08-09 09:53:21 +0200

    Removed unused imports

--- Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/MainWindow.xaml.cs
+++ Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/MainWindow.xaml.cs
@@ -7,11 +7,9 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Color = System.Drawing.Color;
 using Pen = System.Drawing.Pen;
-using PixelFormat = System.Windows.Media.PixelFormat;
 using DrawingPoint = System.Drawing.Point;
 using System.Windows.Media.Media3D;
 
