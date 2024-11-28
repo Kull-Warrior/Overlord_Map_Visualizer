dafb960a9360dbb7be8fdf5620e8e9ba7515cdcd
Kull-Warrior <tobias992000@gmail.com>
2024-11-28 20:47:29 +0100

    Merge horizontal and vertical rotation

--- Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/resources/classes/TrackBall.cs
+++ Overlord_Map_Visualizer_Project/Overlord_Map_Visualizer/resources/classes/TrackBall.cs
@@ -15,8 +15,6 @@ namespace Overlord_Map_Visualizer
 
         // The state of the trackball
         private bool IsEnabled;
-        private bool IsRotatingVertically = false;
-        private bool IsRotatingHorizontally = false;
 
         // The state of the current drag
         private Point InitialPoint; // Initial point of drag
@@ -41,8 +39,6 @@ namespace Overlord_Map_Visualizer
         public void Attach(FrameworkElement element)
         {
             element.MouseMove += MouseMoveHandler;
-            element.MouseLeftButtonDown += MouseLeftButtonDownHandler;
-            element.MouseLeftButtonUp += MouseLeftButtonUpHandler;
             element.MouseRightButtonDown += MouseRightButtonDownHandler;
             element.MouseRightButtonUp += MouseRightButtonUpHandler;
             element.MouseWheel += OnMouseWheel;
@@ -76,63 +72,24 @@ namespace Overlord_Map_Visualizer
                 double diffY = InitialPoint.Y - e.MouseDevice.GetPosition(el).Y;
                 ProjectionCamera camera = (ProjectionCamera)SlaveViewPorts[0].Camera;
 
-                if (IsRotatingVertically)
-                {
-                    double angleD = u * diffY;
+                double angleY = u * diffY;
 
-                    // Cross Product gets a vector that is perpendicular to the passed in vectors (order does matter, reverse the order and the vector will point in the reverse direction)
-                    Vector3D cp = Vector3D.CrossProduct(camera.UpDirection, camera.LookDirection);
-                    cp.Normalize();
+                // Cross Product gets a vector that is perpendicular to the passed in vectors (order does matter, reverse the order and the vector will point in the reverse direction)
+                Vector3D cp = Vector3D.CrossProduct(camera.UpDirection, camera.LookDirection);
+                cp.Normalize();
 
-                    Matrix3D m = new Matrix3D();
-                    m.Rotate(new Quaternion(cp, -angleD)); // Rotate about the vector from the cross product
-                    camera.LookDirection = m.Transform(camera.LookDirection);
-                }
+                Matrix3D mY = new Matrix3D();
+                mY.Rotate(new Quaternion(cp, angleY)); // Rotate about the vector from the cross product
+                camera.LookDirection = mY.Transform(camera.LookDirection);
 
-                if (IsRotatingHorizontally)
-                {
-                    double angleD = u * diffX;
+                double angleX = u * diffX;
 
-                    Matrix3D m = new Matrix3D();
-                    m.Rotate(new Quaternion(camera.UpDirection, -angleD)); // Rotate about the camera's up direction to look left/right
-                    camera.LookDirection = m.Transform(camera.LookDirection);
-                }
+                Matrix3D mX = new Matrix3D();
+                mX.Rotate(new Quaternion(camera.UpDirection, angleX)); // Rotate about the camera's up direction to look left/right
+                camera.LookDirection = mX.Transform(camera.LookDirection);
 
                 InitialPoint = e.MouseDevice.GetPosition(el);
             }
-        }
-
-        private void MouseLeftButtonDownHandler(object sender, MouseButtonEventArgs e)
-        {
-            if (!Enabled)
-            {
-                return;
-            }
-
-            e.Handled = true;
-
-            UIElement el = (UIElement)sender;
-            Mouse.OverrideCursor = Cursors.ScrollWE;
-            InitialPoint = e.MouseDevice.GetPosition(el);
-
-            IsRotatingVertically = false;
-            IsRotatingHorizontally = true;
-
-            el.CaptureMouse();
-        }
-
-        private void MouseLeftButtonUpHandler(object sender, MouseButtonEventArgs e)
-        {
-            if (!IsEnabled)
-            {
-                return;
-            }
-
-            e.Handled = true;
-
-            UIElement el = (UIElement)sender;
-            Mouse.OverrideCursor = null;
-            el.ReleaseMouseCapture();
         }
 
         private void MouseRightButtonDownHandler(object sender, MouseButtonEventArgs e)
@@ -145,11 +102,8 @@ namespace Overlord_Map_Visualizer
             e.Handled = true;
 
             UIElement el = (UIElement)sender;
-            Mouse.OverrideCursor = Cursors.ScrollNS;
+            Mouse.OverrideCursor = Cursors.ScrollAll;
             InitialPoint = e.MouseDevice.GetPosition(el);
-
-            IsRotatingVertically = true;
-            IsRotatingHorizontally = false;
 
             el.CaptureMouse();
         }
